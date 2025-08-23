28c3f755c7ea4e7ae019b747a7ca1e257a037346
Simbalol1x <cromwellsimba14@gmail.com>
2025-08-23 15:34:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js (Final Corrected Version - Student Leaderboard Included)
 
 // --- 1. GLOBAL STATE & CONSTANTS ---
-const API_BASE_URL = 'https://classroom-mj8ekrnjs-simbalol1xs-projects.vercel.app';
+const API_BASE_URL = 'https://classroom-hjmsogmxk-simbalol1xs-projects.vercel.app';
 let currentUser = null;
 let isEditingClass = false;
 let taughtClassesCache = [];
@@ -629,6 +629,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if(copyrightYearEl) copyrightYearEl.textContent = `© ${new Date().getFullYear()} Classroom Pro.`;
     console.log("Classroom Pro App Initialized (Final with Notification Logic)");
 });
+
 
 
 
